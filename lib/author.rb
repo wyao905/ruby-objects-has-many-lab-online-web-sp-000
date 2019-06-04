9415cdff42c1fe34956b70e837a6157a4f22a291
Warren Yao <warrenyca905@gmail.com>
2019-06-04 01:23:34 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author
+  attr_accessor :name
+  
+  @@authors = []
+  
+  def initialize(name)
+    @name = name
+    @post_list = []
+    @@authors << self
+  end
+  
+  def posts
+    @post_list
+  end
+  
+  def add_post(posting)
+    posting.artist = self
+    posts << posting
+  end
+  
+  def add_post_by_name(post_name)
+    posts = Post.new(post_name)
+    song.artist = self
+    songs << song
+  end
+  
+  def self.song_count
+    count = 0
+    @@artists.each {|artist| count += artist.songs.length}
+    count
+  end
+end
